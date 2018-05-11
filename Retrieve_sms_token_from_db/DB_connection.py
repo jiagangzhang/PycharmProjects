4bdbc60cc4b491f231444dcffb46028ca8108386
Jiagang Zhang <jiagangzhang@mymm.com>
2018-05-11 23:31:52 +0800

    basic ssh and db

--- Retrieve_sms_token_from_db/DB_connection.py
+++ Retrieve_sms_token_from_db/DB_connection.py
@@ -1,21 +1,39 @@
-import MySQLdb
-from sshtunnel import SSHTunnelForwarder
+import pymysql
+from sshtunnel import SSHTunnelForwarder, create_logger
+
+
+class DbConnect(object):
+    """docstring for DbConnect"""
+    def __init__(self, host='', port=20, ):
+        super(DbConnect, self).__init__()
+        self.host = host
+        self.port = port
 
 
 def connect_db():
     with SSHTunnelForwarder(
-            ('sshhost.domain.com', 22),  # B机器的配置
-            ssh_password="sshpasswd",
-            ssh_username="sshusername",
-            remote_bind_address=('mysqlhost.domain.com', 3306)) as server:  # A机器的配置
+            ('mm-ts-next.chinaeast.cloudapp.chinacloudapi.cn', 1021),  # 远端server ssh ip 和端口
+            # logger=create_logger(loglevel=1),  # sshtunnel debug log 打印输出
+            ssh_username="ubuntu",
+            ssh_pkey="loadtesters_rsa-2.pem",
+            remote_bind_address=('127.0.0.1', 3306),  # 远端server ip 和 db 端口
+            local_bind_address=('127.0.0.1', 3306)  # 本地连接db的端口
+            ) as server:
 
-        conn = MySQLdb.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
-                               port=server.local_bind_port,
-                               user='user',
-                               passwd='password',
-                               db='dbname')
-        cursor = conn.cursor()
-        cursor.execute("show tables")
         print(server.local_bind_address)
-        print(len(cursor.fetchall()))
+        print(server.local_bind_port)
+        db = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
+                             port=server.local_bind_port,
+                             user='root',
+                             password='3nations',
+                             database='mm')
+        cursor = db.cursor()
+        cursor.execute("show tables")
+        sql = 'select * from `User` limit 101'
+        cursor.execute(sql)
+        print(cursor.rowcount)
+        # print(len(cursor.fetchall()))
         server.stop()
+
+if __name__ == '__main__':
+    connect_db()
